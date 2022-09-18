fae5cd36773d4f4d2af8e1670cc88ef5644a3be4
Gökdeniz YILMAZ <93597191+geokdeniz@users.noreply.github.com>
2022-09-18 23:47:33 +0300

    C sharp arayüz
    arayüzün açıklaması

---
+++ split/split/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = "geokdenizyilmaz28@gmail.com";
+            string[] a = email.Split('@');
+            Console.WriteLine(a[0]+" "+a[1]);
+            Console.ReadKey();
+        }
+    }
+}
